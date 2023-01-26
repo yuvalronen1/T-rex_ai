56a165ea9699d68d659612877f5a52252012ba02
yuvalronen11 <rabinai.yuval@gmail.com>
2023-01-26 15:04:16 +0200

    put graphics in main. added background, player and obstacles

--- actuallyGoodGameForReal/logic.py
+++ actuallyGoodGameForReal/logic.py
@@ -1,7 +1,7 @@
 from random import choice
 from random import randrange as rnd
 
-obstacles = ["o1", "o2", "o3", "o4", "o5", "o6", "b1"]
+obstacles = ["o1", "o2", "o3", "o4", "o5", "o6", "b"]
 
 class Logic():
 
@@ -9,25 +9,30 @@ class Logic():
         self.intialSpeed = speed
         self.speed = speed
         self.score = 1
-        self.state = 'r'
         self.chrashed = False
         self.jumping = False
         self.fast_fall = False
-        self.height = 110
-
-
-
-    def newObs(self):
-        obs = (rnd(600, 600+500), 130)
-        obast = choice(obstacles)
-        if obast in  ["o4", "o5", "o6"]:obs = (obs[0], 115)
-        if obast == "b1":
-            obs1 = (obs1[0], choice([80, 60]))
-            isbird = True
-        else: isbird = False
-        
-        return obs, obast, isbird
     
     def updateSpeedScore(self):
         self.speed += 0.001
         self.score += 0.1
+
+class Obstacle():
+    def __init__(self):
+        self.pos = (rnd(600, 600+500), 130)
+        self.sort = choice(obstacles)
+        if self.sort in  ["o4", "o5", "o6"]: self.pos = (self.pos[0], 115)
+        if self.sort == "b":
+            self.pos = (self.pos[0], choice([80, 60]))
+            self.isbird = True
+        else: self.isbird = False
+    
+    def move(self, speed):
+        self.pos = (self.pos[0] - speed, self.pos[1])
+
+
+class Player():
+    def __init__(self):
+        self.state = 'r'
+        self.height = 110
+        self.POSITION = 5
